0f90834b639a3898cc7be8e71f4cc62ab211b52f
AbiterVX <32676474+AbiterVX@users.noreply.github.com>
2021-05-12 14:58:26 +0800

    update doc

--- hostmonitor_client/src/main/java/com/hust/hostmonitor_client/utils/DataSampler.java
+++ hostmonitor_client/src/main/java/com/hust/hostmonitor_client/utils/DataSampler.java
@@ -11,7 +11,23 @@ import oshi.util.Util;
 import java.text.DecimalFormat;
 import java.util.List;
 
+/*
+    参考文档：http://oshi.github.io/oshi/oshi-core/apidocs/oshi/hardware/package-summary.html
 
+    cpu利用率：两次间隔采样，并计算idle比例
+    memory利用率：globalMemory.getAvailable() / getTotal
+    磁盘占用率：计算每个盘的单独容量以及当前使用量。
+    磁盘iops：两次采样，io个数/时间段=iops
+    磁盘io速率：两次采样，磁盘读写数据量/时间段
+    网络：两次采样，计算收发速率
+
+硬件：
+    CPU：类型，核数，温度
+    磁盘：类型，总量
+    GPU：类型
+    操作系统：类型，版本
+
+*/
 public class DataSampler {
     SystemInfo systemInfo = new SystemInfo();
     public DataSampler(){
@@ -21,6 +37,11 @@ public class DataSampler {
     }
 
     public void sample(){
+
+
+
+
+
         GlobalMemory globalMemory = systemInfo.getHardware().getMemory();
 
         long totalByte = globalMemory.getTotal();
