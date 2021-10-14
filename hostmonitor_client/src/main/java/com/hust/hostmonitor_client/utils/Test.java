08d5a7303bbfc3e9b3fe2237c30ca73f9cfb296e
AbiterVX <32676474+AbiterVX@users.noreply.github.com>
2021-10-14 21:13:15 +0800

    smart信息采集-cmd/shell方式获取

--- hostmonitor_client/src/main/java/com/hust/hostmonitor_client/utils/Test.java
+++ hostmonitor_client/src/main/java/com/hust/hostmonitor_client/utils/Test.java
@@ -1,12 +1,15 @@
 package com.hust.hostmonitor_client.utils;
 
 import com.alibaba.fastjson.JSONObject;
+import com.csvreader.CsvWriter;
 import com.vnetpublishing.java.suapp.SU;
 import com.vnetpublishing.java.suapp.SuperUserApplication;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 
@@ -46,51 +49,169 @@ class CmdExecutor{
 
 public class Test  {
     public static void main(String[] args) {
-//        //判断OS类型
-//        SystemInfo systemInfo = new SystemInfo();
-//        OperatingSystem os = systemInfo.getOperatingSystem();
-//        String osName = os.toString();
-//
-//        //输入的指令
-//        String speedTestCmd;
-//        if(osName.contains("Microsoft Windows")){
-//            speedTestCmd = "winsat disk";
-//        }
-//        else{
-//            String cmdFilePath=System.getProperty("user.dir")+"/SpeedTest.sh";
-//
-//            speedTestCmd = cmdFilePath;
-//
-//                    //"(LANG=C dd if=/dev/zero of=benchtest_$$ bs=64k count=16k conv=fdatasync ) 2>&1 | awk -F, '{io=$NF} END { print io}' | sed 's/^[ \\t]*//;s/[ \\t]*$//'  && " +
-//                    //"(LANG=C dd if=benchtest_$$ of=/dev/null bs=64k count=16k conv=fdatasync) 2>&1 | awk -F, '{io=$NF} END { print io}' | sed 's/^[ \\t]*//;s/[ \\t]*$//'   && " +
-//                    //"rm -f benchtest_$$ ";
-//        }
-//        //输出
-//        String readSpeed = "";
-//        String writeSpeed = "";
-//
-//        //处理输出数据
-//        List<String> cmdResult = new CmdExecutor(speedTestCmd).cmdResult;
-//        if(cmdResult!=null){
-//            if(osName.contains("Microsoft Windows")){
-//                for(String currentOutput: cmdResult){
-//                    String[] rawData = currentOutput.split("\\s+");
-//                    if(currentOutput.contains("Disk  Sequential 64.0 Read")){
-//                        readSpeed = rawData[5] +" "+ rawData[6];
-//                    }
-//                    else if(currentOutput.contains("Disk  Sequential 64.0 Write")){
-//                        writeSpeed = rawData[5] +" "+ rawData[6];
-//                    }
-//                }
-//            }
-//            else{
-//                writeSpeed = cmdResult.get(0);
-//                readSpeed = cmdResult.get(1);
-//            }
-//        }
-//
-//        //结果
-//        System.out.println(readSpeed);
-//        System.out.println(writeSpeed);
+        //判断OS类型
+        String osName = "";
+        {
+            {
+                List<String> cmdResult = new CmdExecutor("cmd /c ver").cmdResult;
+                if(cmdResult.size() == 2){
+                    osName = cmdResult.get(1);
+                }
+            }
+            if(osName.equals("")){
+                List<String> cmdResult = new CmdExecutor("cat /proc/version").cmdResult;
+                osName = cmdResult.get(0);
+            }
+        }
+
+        //获取硬盘smart数据
+        JSONObject diskData = new JSONObject();
+        {
+            //获取硬盘名
+            List<String> diskList = new ArrayList<>();
+            {
+                String getDiskListCmd = "";
+                if(osName.contains("Microsoft Windows")){
+                    getDiskListCmd = "wmic logicaldisk get deviceid";
+                    List<String> cmdResult = new CmdExecutor(getDiskListCmd).cmdResult;
+                    cmdResult.remove(0);
+                    for(String currentStr:cmdResult){
+                        if(!currentStr.equals("")){
+                            diskList.add(currentStr.trim());
+                        }
+                    }
+                }
+                else{
+                    getDiskListCmd = "lsblk -bnd";
+                    List<String> cmdResult = new CmdExecutor(getDiskListCmd).cmdResult;
+                    for(String currentStr:cmdResult) {
+                        if(currentStr.startsWith("Disk")){
+                            String[] rawData = currentStr.split("\\s+");
+                            diskList.add(rawData[1].substring(0, rawData[1].length()-1));
+                        }
+                    }
+                }
+            }
+
+            //以Json格式存数据
+            String smartDiskInfoCmd = "smartctl -i ";
+            String smartDataSampleCmd = "smartctl -A ";
+            for(String currentDiskName: diskList){
+                JSONObject currentDiskData = new JSONObject();
+                {
+                    List<String> cmdResult = new CmdExecutor(smartDiskInfoCmd + currentDiskName).cmdResult;
+                    for (int i = 0; i < 4; i++) {
+                        cmdResult.remove(0);
+                    }
+                    cmdResult.remove(cmdResult.size() - 1);
+                    for(String currentOutput: cmdResult){
+                        String[] rawData = currentOutput.split(":\\s+");
+                        currentDiskData.put(rawData[0],rawData[1]);
+                    }
+                }
+
+                String serialNumber = currentDiskData.getString("Serial Number");
+                if(!diskData.containsKey(serialNumber)){
+                    diskData.put(serialNumber,currentDiskData);
+                    JSONObject smartData = new JSONObject();
+                    {
+                        List<String> cmdResult = new CmdExecutor(smartDataSampleCmd + currentDiskName).cmdResult;
+                        for (int i = 0; i < 7; i++) {
+                            cmdResult.remove(0);
+                        }
+                        for(String currentOutput: cmdResult){
+                            if(!currentOutput.equals("")){
+                                currentOutput = currentOutput.trim();
+                                String[] rawData = currentOutput.split("\\s+");
+                                JSONObject currentSmart = new JSONObject();
+                                currentSmart.put("ATTRIBUTE_NAME",rawData[1]);
+                                currentSmart.put("VALUE",rawData[3]);
+                                currentSmart.put("RAW_VALUE",rawData[9]);
+                                smartData.put(rawData[0],currentSmart);
+                            }
+                        }
+                        currentDiskData.put("SmartData",smartData);
+                    }
+                }
+            }
+        }
+        System.out.println(diskData.toString());
+
+        //获取当前时间:
+        String currentDate = "";
+        String pt_d = "";
+        {
+            Date date = new Date();
+            {
+                SimpleDateFormat sdf = new SimpleDateFormat();
+                sdf.applyPattern("yyyy/MM/dd HH:mm");
+                currentDate = sdf.format(date);
+            }
+            {
+                SimpleDateFormat sdf = new SimpleDateFormat();
+                sdf.applyPattern("yyyyMMdd");
+                pt_d = sdf.format(date);
+            }
+        }
+
+        //写文件
+        String sampleDataFilePath=System.getProperty("user.dir")+"/DiskPredict/client/data.csv";
+        CsvWriter csvWriter = new CsvWriter(sampleDataFilePath,',', Charset.forName("GBK"));
+        try {
+            //Smart属性个数
+            int smartCount = 256;
+            //获取表头
+            List<String> headers = new ArrayList<>();
+            {
+                String[] staticHeaders = {"date", "serial_number", "model", "serialAlternative", "failure", "is_ssd", "pt_d"};
+                for(String staticHeader:staticHeaders){
+                    headers.add(staticHeader);
+                }
+
+                String[] smartTagAttributes = {"_normalized","_raw"};
+                for(int i=0;i<smartCount;i++){
+                    for(String attribute: smartTagAttributes){
+                        headers.add("smart_"+ Integer.toString(i) + attribute);
+                    }
+                }
+            }
+
+            //写入header头
+            csvWriter.writeRecord(headers.toArray(new String[0]));
+
+            //写入数据
+            Set<String> serialNumberList = diskData.keySet();
+            for(String serialNumber:serialNumberList){
+                JSONObject currentDiskData = diskData.getJSONObject(serialNumber);
+                List<String> rowData = new ArrayList<>();
+                {
+                    rowData.add(currentDate);
+                    rowData.add(serialNumber);
+                    rowData.add(currentDiskData.getString("Device Model"));
+                    rowData.add("");
+                    rowData.add("0");
+                    rowData.add("0");
+                    rowData.add(pt_d);
+                    JSONObject smartData = currentDiskData.getJSONObject("SmartData");
+                    //@Todo 0到255
+                    for(int i=0;i<smartCount;i++){
+                        JSONObject currentSmart = smartData.getJSONObject(Integer.toString(i));
+                        if(currentSmart != null){
+                            rowData.add(currentSmart.getString("VALUE"));
+                            rowData.add(currentSmart.getString("RAW_VALUE"));
+                        }
+                        else{
+                            rowData.add("");
+                            rowData.add("");
+                        }
+                    }
+                }
+                csvWriter.writeRecord(rowData.toArray(new String[0]));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            csvWriter.close();
+        }
     }
 }
